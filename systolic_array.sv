75803cd9b5640ce86785415cf5977b0ba3603589
anmehta4 <anmehta4@wisc.edu>
2022-09-20 18:48:06 -0500

    Made final changes

--- systolic_array.sv
+++ systolic_array.sv
@@ -13,6 +13,38 @@ module systolic_array
    output signed [BITS_C-1:0] Cout [DIM-1:0]
 );
 
+   genvar row, col;
 
+   logic signed [BITS_AB-1:0] Aval [DIM:0][DIM:0];
+   logic signed [BITS_AB-1:0] Bval [DIM:0][DIM:0];
+   logic signed [BITS_C-1: 0] Cval [DIM-1:0][DIM-1:0];
+  
+   generate 
+      for(row = 0 ; row < DIM; row++) begin
+          assign Aval[row][0] = A[row];
+      end
+      
+      for(col = 0; col < DIM; col++) begin
+          assign Bval[0][col] = B[col];
+      end
+
+      for (row = 0; row < DIM; row++) begin
+         for (col = 0; col < DIM; col++) begin
+	     tpumac tpu(.clk(clk), 
+			.rst_n(rst_n), 
+			.WrEn(WrEn && (row == Crow)), 
+			.en(en),
+                 	.Ain(Aval[row][col]), 
+			.Bin(Bval[row][col]), 
+			.Cin(Cin[col]),
+                 
+			.Aout(Aval[row][col+1]), 
+			.Bout(Bval[row+1][col]), 
+			.Cout(Cval[row][col]));
+         end
+      end
+   endgenerate
+
+   assign Cout = Cval[Crow];
 endmodule
    
